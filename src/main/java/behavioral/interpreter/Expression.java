a1033878b00b2b1254c3e70b223c1b61475330c2
Aliaksei Halkin <aliaksei.halkin@abfprogramming.com>
2022-04-12 16:17:12 +0300

    added   patterns

---
+++ src/main/java/behavioral/interpreter/Expression.java
@@ -0,0 +1,57 @@
+package behavioral.interpreter;
+
+/**
+ * +
+ * Позволяет определить грамматику простого языка для проблемной области.
+ */
+public interface Expression {
+    String interpret(Context context);
+}
+
+class Context {
+    public String getLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+    public String getUpperCase(String s) {
+        return s.toUpperCase();
+    }
+}
+
+class LowerExpression implements Expression {
+    private String s;
+
+    LowerExpression(String s) {
+        this.s = s;
+    }
+
+    public String interpret(Context context) {
+        return context.getLowerCase(s);
+    }
+}
+
+class UpperExpression implements Expression {
+    private String s;
+
+    public UpperExpression(String s) {
+        this.s = s;
+    }
+
+    public String interpret(Context context) {
+        return context.getUpperCase(s);
+    }
+}
+
+class InterpreterTest {//тест
+
+    public static void main(String[] args) {
+        String str = "TesT";
+        Context context = new Context();
+        Expression loverExpression = new LowerExpression(str);
+        str = loverExpression.interpret(context);
+        System.out.println(str);
+        Expression upperExpression = new UpperExpression(str);
+        str = upperExpression.interpret(context);
+        System.out.println(str);
+    }
+}
---
+++ src/main/java/behavioral/memento/Memento.java
@@ -0,0 +1,55 @@
+package behavioral.memento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Позволяет сохранить текущее состояние объекта, позже это состояние можно восстановить. Не нарушает инкапсуляцию.
+ */
+public class Memento {
+    private String name;
+    private int age;
+
+    public Memento(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+        System.out.println(String.format("create: name = %s, age = %s", name, age));
+    }
+
+    public Memento save() {
+        System.out.println(String.format("save: name = %s, age = %s", name, age));
+        return new Memento(name, age);
+    }
+
+    public void restore(Memento memento) {
+        name = memento.getName();
+        age = memento.getAge();
+        System.out.println(String.format("restore: name = %s, age = %s", name, age));
+    }
+}
+
+class SaveUser {
+    private List<Memento> list = new ArrayList<Memento>();
+
+    public void add(Memento memento) {
+        list.add(memento);
+    }//todo will add main and some methods here
+}
